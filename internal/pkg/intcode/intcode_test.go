038c7b080cab266ed71fbdf3eaf47b84108c8171
Jay Senich <1821282+jsenich@users.noreply.github.com>
2022-10-17 23:18:22 -0400

    Clean up intcode tests

--- internal/pkg/intcode/intcode_test.go
+++ internal/pkg/intcode/intcode_test.go
@@ -1,29 +1,83 @@
 package intcode
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
-func TestComputer_ExecuteProgram(t *testing.T) {
+func IntSliceToString(vals []int) string {
+	stringSlice := make([]string, len(vals))
+
+	for i, v := range vals {
+		stringSlice[i] = strconv.Itoa(v)
+	}
+
+	return strings.Join(stringSlice, ",")
+}
+
+func TestComputer_ExecuteProgram_Day02(t *testing.T) {
 	tests := []struct {
+		name    string
+		program string
+		args    []int
+		want    string
+	}{
+		{
+			name:    "add operation returns expected state (1 + 1 = 2)",
+			program: "1,0,0,0,99", want: "2,0,0,0,99",
+		},
+		{
+			name:    "multiply operation returns expected state (3 * 2 = 6)",
+			program: "2,3,0,3,99", want: "2,3,0,6,99",
+		},
+		{
+			name:    "multiply operation returns expected state (99 * 99 = 9801)",
+			program: "2,4,4,5,99,0", want: "2,4,4,5,99,9801",
+		},
+		{
+			name:    "multiple operations return expected state",
+			program: "1,1,1,4,99,5,6,0,99", want: "30,1,1,4,2,5,6,0,99",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewComputer([]byte(tt.program))
+			c.ExecuteProgram(tt.args...)
+
+			got := IntSliceToString(c.Memory)
+
+			if got != tt.want {
+				t.Errorf("got %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestComputer_ExecuteProgram_Day05(t *testing.T) {
+	tests := []struct {
+		name                 string
 		program              string
 		args                 []int
 		want                 int
 		checkPosition        int
 		returnDiagnosticCode bool
 	}{
-		{program: "1,0,0,0,99", want: 2, checkPosition: 0},
-		{program: "2,3,0,3,99", want: 2, checkPosition: 0},
-		{program: "2,4,4,5,99,0", want: 2, checkPosition: 0},
-		{program: "1,1,1,4,99,5,6,0,99", want: 30},
-		{program: "3,0,4,0,99", args: []int{55}, want: 55, returnDiagnosticCode: true},
-		{program: "3,0,4,0,99", args: []int{2}, want: 2, returnDiagnosticCode: true},
-		{program: "3,0,4,0,99", args: []int{123}, want: 123, returnDiagnosticCode: true},
-		{program: "1002,4,3,4,33", want: 99, checkPosition: 4},
-		{program: "1101,100,-1,4,0", want: 99, checkPosition: 4},
+		{
+			name:    "returns diagnostic code output equal to the input",
+			program: "3,0,4,0,99", args: []int{55}, want: 55, returnDiagnosticCode: true,
+		},
+		{
+			name:    "parameter modes with multiply operation sets value at expected postition (33 * 3 = 99)",
+			program: "1002,4,3,4,33", want: 99, checkPosition: 4,
+		},
+		{
+			name:    "parameter modes with add operation sets value at expected postition (100 + -1 = 99)",
+			program: "1101,100,-1,4,0", want: 99, checkPosition: 4,
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.program, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			c := NewComputer([]byte(tt.program))
 			c.ExecuteProgram(tt.args...)
 			var got int
